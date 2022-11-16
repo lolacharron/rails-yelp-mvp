656cfecce8e5883f55d7d3586b932d37c81a1796
Lola Charron <lolacharron.surf@hotmail.fr>
2022-11-16 15:45:57 +0100

    small

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "restaurants", to: "restaurants#index"
-  get "restaurant/:id", to: "restaurants#show", as: :restaurant
+  get "restaurant/:restaurant_id", to: "restaurants#show", as: :restaurant
   get "restaurants/new", to: "restaurants#new"
   post "restaurants", to: "restaurants#create"
-  get "restaurants/:id", to: "restaurants#show"
-  get "restaurant/reviews/new", to: "reviews#new"
-  get "restaurant/reviews", to: "reviews#create"
+  get "restaurants/:restaurant_id", to: "restaurants#show"
+  get "restaurants/:restaurant_id/reviews/new", to: "reviews#new"
+  get "restaurants/:restaurant_id/reviews", to: "reviews#create"
   # Defines the root path route ("/")
   # root "articles#index"
 end
